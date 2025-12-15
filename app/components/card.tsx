96e510ad9b1790595f8483b197f0448bd8a7ac37
este <esteban.h0207@gmail.com>
2025-12-15 22:09:14 +0100

    add card to main page

---
+++ app/components/card.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+interface CardProps {
+    href: string;
+    hrefimg: string;
+    title: string;
+    desc: string;
+
+}
+
+export default function Card({ href, title, desc, hrefimg }: CardProps)  {
+  return (
+    <div className="flex min-h-full items-center justify-center bg-transparent font-sans">
+      <Link href={href} className="w-80 overflow-hidden rounded-2xl bg-white shadow-lg dark:bg-zinc-900 block transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+        <img
+          src={hrefimg}
+          alt="Card image"
+          className="h-64 w-full object-cover"
+        />
+        <div className="p-6">
+          <h3 className="text-xl font-semibold text-zinc-900 dark:text-white">
+        {title}
+          </h3>
+          <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+        {desc}
+          </p>
+        </div>
+      </Link>  
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 
 import Header from "./components/header";
+import Card from "./components/card";
 
 
 export default function Home() {
   return (
     <div className="min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black">
       <Header />
+      <div className="flex flex-wrap justify-center gap-8 py-12 bg-brown-100 dark:bg-brown-800">
+        <Card href="/contact" hrefimg="image1.jpg" title="Test1" desc="papapa"/>
+        <Card href="/contact" hrefimg="image2.jpg" title="Test2" desc="papapa"/>
+        <Card href="/contact" hrefimg="image3.jpg" title="Test3" desc="papapa"/>
+      </div>
     </div>
   );
 }
